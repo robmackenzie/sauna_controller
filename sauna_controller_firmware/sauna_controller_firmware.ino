c25e1c7beddeb8e4162c9efbbb1125906687c86b
Rob MacKenzie <rmackenzie@ea.com>
2025-04-24 20:55:51 -0700

    Working MQTT and everything really. A few TODOs left

--- sauna_controller_firmware/sauna_controller_firmware.ino
+++ sauna_controller_firmware/sauna_controller_firmware.ino
@@ -1,23 +1,208 @@
 #include "M5Dial.h"
 #include "temperature_font.h"
+#include "logos.h"
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+#include <TaskScheduler.h>
+
 #define DEFAULT_BACKGROUND_COLOUR 0x0A2D
 #define SAUNA_GPIO_PIN 1
 
 M5Canvas img(&M5Dial.Display);
 time_t target_end_time = 0;
 bool in_temperature_setting_mode = false;
+bool mqttConnected = false;
+bool wifiConnected = false;
+bool isAPMode = false;
+bool heating_element_on = false;
+
 long oldPosition;
-int set_temp = 90;
-bool beeped = false;
-bool beeping = false;
-unsigned long last_beep_time = 0;
-int beep_count = 0;
-bool beep_on = false;
+int target_temp = 90;
+
+//TODO: Fix beeping. It should be it's own function in loop, do it with a queue or something, with channels.
+//TODO: Add in status checks for the status logos
+//TODO: Better wifi/mqtt config. Currently a mix of config'ed wifi, but hardcoded mqtt
+
+WiFiManager wifiManager;
+WiFiManagerParameter custom_mqtt_server("server", "mqtt server");
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+Scheduler ts;
+
+void setup_mqtt() {
+  mqttClient.setServer("homeassistant.local", 1883);
+  mqttClient.setCallback(mqttCallback);
+  if (wifiConnected) {
+    mqttConnected = mqttClient.connect("sauna_m5dial", "device", "local_device");
+    if (mqttConnected) {
+      mqttClient.subscribe("sauna/control/#");
+    }
+  }
+}
+
+void show_boot_screen() {
+  M5Dial.Display.fillScreen(BLACK);
+  M5Dial.Display.setTextDatum(middle_center);
+  M5Dial.Display.setTextColor(WHITE);
+  M5Dial.Display.drawString("Booting...", 120, 120, 4);
+}
+
+void connect_wifi() {
+  wifiManager.setConfigPortalBlocking(true);
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.setSaveParamsCallback(wifiManagerSaveParamsCallback);
+  isAPMode = !wifiManager.autoConnect("Sauna Controller Setup");
+  wifiConnected = WiFi.isConnected();
+}
+
+void wifiManagerSaveParamsCallback() {
+  Serial.println("Get Params:");
+  Serial.print(custom_mqtt_server.getID());
+  Serial.print(" : ");
+  Serial.println(custom_mqtt_server.getValue());
+}
+
+void render_ui() {
+  img.fillSprite(BLACK);
+  int background_colour = should_sauna_be_active() ? TFT_MAROON : DEFAULT_BACKGROUND_COLOUR;
+  img.fillRect(0, 0, 240, 130, background_colour);
+
+  // Wifi/MQTT status icons
+  img.drawBitmap(120-50, 20, wifi_logo, 24, 17, isAPMode ? ORANGE : (wifiConnected ? GREEN : RED));
+  img.drawBitmap(120+50-12, 20, mqtt_logo, 17, 17, mqttConnected ? GREEN : RED);
+
+  img.setTextColor(TFT_LIGHTGRAY, background_colour);
+  img.drawString(String(get_current_temp()) + char(176), 120 - 45, 80, &FreeSans18pt7b_mod);
+  if (in_temperature_setting_mode) img.setTextColor(RED, background_colour);
+  img.drawString(String(target_temp) + char(176), 120 + 45, 80, &FreeSans18pt7b_mod);
+
+  img.setTextColor(WHITE, background_colour);
+  img.drawString("Current", 120 - 45, 110, 2);
+  if (in_temperature_setting_mode) img.setTextColor(RED, background_colour);
+  img.drawString("Target", 120 + 45, 110, 2);
+
+  img.setTextColor(WHITE, BLACK);
+  time_t now = time(nullptr);
+  unsigned long delta_seconds = (target_end_time > now) ? (target_end_time - now) : 0;
+  uint8_t seconds = delta_seconds % 60;
+  unsigned long mins = (delta_seconds - seconds) / 60;
+  
+  char time_string[7];
+  sprintf(time_string, "%02lu:%02d", mins, seconds);
+  img.drawString(time_string, 120, 170, 7);
+
+  img.setTextColor(ORANGE, BLACK);
+  img.drawString("RESET", 120, 232, 2);
+
+  img.pushSprite(0, 0);
+}
+
+int get_current_temp() {
+  // Stub - replace with real sensor value later
+  return 20;
+}
+
+void reset() {
+  target_end_time = 0;
+  in_temperature_setting_mode = false;
+  target_temp=0;
+  mqttPublisherCallback();
+}
+
+void handle_button() {
+  if (M5Dial.BtnA.wasPressed()) {
+    M5Dial.Speaker.tone(2800, 100);
+    reset();
+  }
+}
+
+bool should_sauna_be_active() {
+  return target_end_time > time(nullptr);
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  String message;
+  for (unsigned int i = 0; i < length; i++) message += (char)payload[i];
+  if (String(topic) == "sauna/control/target_temp") target_temp = message.toInt();
+  else if (String(topic) == "sauna/control/start") {
+    target_end_time = time(nullptr) + message.toInt();
+  } else if (String(topic) == "sauna/control/stop") {
+    reset();
+  }
+}
+static m5::touch_state_t touch_prev_state;
+void handle_touch() {
+  const auto touch_sense = M5Dial.Touch.getDetail();
+  if (touch_prev_state != touch_sense.state) {
+    touch_prev_state = touch_sense.state;
+    if (touch_sense.wasPressed()) {
+      in_temperature_setting_mode = !in_temperature_setting_mode;
+      M5Dial.Speaker.tone(3000, 100);
+    }
+  }
+}
+
+void handle_dial() {
+  long newPosition = M5Dial.Encoder.read();
+  if (newPosition != oldPosition && newPosition % 4 == 0) {
+    bool spun_dial_up = newPosition > oldPosition;
+    M5Dial.Speaker.tone(3600, 30);
+    if (in_temperature_setting_mode) {
+      if (spun_dial_up) target_temp++;
+      else target_temp--;
+    } else {
+      time_t now = time(nullptr);
+      if (!should_sauna_be_active()) target_end_time = now;
+      if (spun_dial_up) target_end_time += 60 * 10;
+      else target_end_time = (target_end_time < now + 60 * 5) ? now : target_end_time - 60 * 10;
+    }
+  mqttPublisherCallback();
+  }
+  oldPosition = newPosition;
+}
+
+void mqttPublisherCallback() {
+  if (!mqttClient.connected()) return;
+
+  mqttClient.publish("sauna/status/target_temp", String(target_temp).c_str());
+  mqttClient.publish("sauna/status/current_temp", String(get_current_temp()).c_str());
+
+  time_t now = time(nullptr);
+  int remaining = (target_end_time > now) ? (target_end_time - now) : 0;
+  mqttClient.publish("sauna/status/remaining_time", String(remaining).c_str());
+  mqttClient.publish("sauna/status/is_on", should_sauna_be_active() ? "true" : "false");
+  mqttClient.publish("sauna/status/is_heating", heating_element_on ? "true" : "false");
+}
+
+void control_sauna() {
+  if (should_sauna_be_active() && target_temp > get_current_temp()) {
+    digitalWrite(SAUNA_GPIO_PIN, HIGH);
+    heating_element_on = true;
+  } else {
+    digitalWrite(SAUNA_GPIO_PIN, LOW);
+    heating_element_on = false;
+  }
+}
+
+Task mqttPublisherTaskFast(3 * 1000, TASK_FOREVER, &mqttPublisherCallback);
+Task mqttPublisherTaskSlow(60 * 1000, TASK_FOREVER, &mqttPublisherCallback);
+Task saunaControlTask(20 * 1000, TASK_FOREVER, &control_sauna);
 
 void setup() {
   auto cfg = M5.config();
-  M5Dial.begin(cfg, true, false);  // config, enableEncoder, enableRFID
-  M5Dial.Rtc.setDateTime({ { 2023, 10, 25 }, { 15, 56, 56 } });
+  M5Dial.begin(cfg, true, false);
+  show_boot_screen();
+  connect_wifi();
+  setup_mqtt();
+
+  ts.init();
+  ts.addTask(mqttPublisherTaskFast);
+  ts.addTask(mqttPublisherTaskSlow);
+  mqttPublisherTaskSlow.enable();
+
+  ts.addTask(saunaControlTask);
+  saunaControlTask.enable();
+
   M5Dial.Display.setBrightness(24);
   img.createSprite(240, 240);
   img.setTextDatum(middle_center);
@@ -27,141 +212,24 @@ void setup() {
   delay(200);
 }
 
-bool is_sauna_running() {
-  time_t now = time(nullptr);
-  return target_end_time > now;
-}
-
-void render_ui() {
-  img.fillSprite(BLACK);  // Black Background
-  int background_colour;
-  if (is_sauna_running()) background_colour = TFT_MAROON;
-  else background_colour = DEFAULT_BACKGROUND_COLOUR;
-  img.fillRect(0, 0, 240, 130, background_colour);
-
-  // Temps
-  img.setTextColor(TFT_LIGHTGRAY, background_colour);
-  img.drawString("--", 120 - 45, 80, &FreeSans18pt7b_mod);
-  if (in_temperature_setting_mode) img.setTextColor(RED, background_colour);
-  img.drawString(String(set_temp) + char(176), 120 + 45, 80, &FreeSans18pt7b_mod);
-
-  // Labels for temps
-  img.setTextColor(WHITE, background_colour);
-  img.drawString("Current", 120 - 45, 110, 2);
-  if (in_temperature_setting_mode) img.setTextColor(RED, background_colour);
-  img.drawString("Target", 120 + 45, 110, 2);
-
-  // Main time display
-  img.setTextColor(WHITE, BLACK);
-  time_t now = time(nullptr);
-  unsigned long delta_seconds = (target_end_time > now) ? (target_end_time - now) : 0;
-  uint8_t seconds = delta_seconds % 60;
-  unsigned long mins = (delta_seconds - seconds) / 60;
-  char time_string[7];  // "MM:SS\0" = 6 chars
-  sprintf(time_string, "%02lu:%02d", mins, seconds);
-  img.drawString(time_string, 120, 170, 7);
-
-  //Label the bottom override button
-  img.setTextColor(ORANGE, BLACK);
-  img.drawString("OFF", 120, 232, 2);
-
-  img.pushSprite(0, 0);
-}
-
-void reset() {
-  target_end_time = 0;
-  in_temperature_setting_mode = false;
-  digitalWrite(SAUNA_GPIO_PIN, LOW);
-  beeped = false;
-  beeping = false;
-  beep_count = 0;
-}
-
-void adjust_time(bool up) {
-  time_t now = time(nullptr);
-  if (!is_sauna_running()) {
-    target_end_time = now;
-  }
-  if (up) {
-    target_end_time += 60 * 5;
-  } else {
-    if (target_end_time < now + 60 * 5) {
-      target_end_time = now;
-    } else {
-      target_end_time -= 60 * 5;
-    }
-  }
-}
-
-void control_sauna() {
-  if (is_sauna_running()) {
-    digitalWrite(SAUNA_GPIO_PIN, HIGH);
-    beeped = false;
-    beeping = false;
-    beep_count = 0;
-  } else {
-    digitalWrite(SAUNA_GPIO_PIN, LOW);
-    if (!beeped && target_end_time != 0) {
-      if (!beeping) {
-        beeping = true;
-        last_beep_time = millis();
-        beep_count = 0;
-        beep_on = false;
-      }
-
-      if (beeping) {
-        unsigned long now = millis();
-        if (!beep_on && now - last_beep_time >= 100) {
-          M5Dial.Speaker.tone(2200);
-          beep_on = true;
-          last_beep_time = now;
-        } else if (beep_on && now - last_beep_time >= 200) {
-          M5Dial.Speaker.end();
-          beep_on = false;
-          last_beep_time = now;
-          beep_count++;
-        }
-
-        if (beep_count >= 5) {
-          beeping = false;
-          beeped = true;
-        }
-      }
-    }
-  }
-}
-
-static m5::touch_state_t touch_prev_state;
-
+// LOOP TIME
 void loop() {
   M5Dial.update();
-  if (M5Dial.BtnA.isPressed()) {
-    M5Dial.Speaker.end();
-    M5Dial.Speaker.tone(2800, 100);
-    reset();
-    delay(200);
+  mqttClient.loop();
+  if(should_sauna_be_active()) {
+    mqttPublisherTaskFast.enableIfNot();
+  } else {
+    mqttPublisherTaskFast.disable();
   }
+  ts.execute();  // Run any tasks are scheduled.
+    // Sauna control
+    // MQTT publush
 
-  const auto touch_sense = M5Dial.Touch.getDetail();
-  if (touch_prev_state != touch_sense.state) {
-    touch_prev_state = touch_sense.state;
-    if (touch_sense.wasPressed()) {
-      in_temperature_setting_mode = !in_temperature_setting_mode;
-      M5Dial.Speaker.tone(3000, 100);
-    }
-  }
+  // Handle User intactions with physical button
+  handle_button();
+  handle_touch();
+  handle_dial();
 
-  long newPosition = M5Dial.Encoder.read();
-  if (newPosition != oldPosition && newPosition % 4 == 0) {
-    M5Dial.Speaker.tone(3600, 30);
-    if (in_temperature_setting_mode) {
-      if (newPosition > oldPosition) set_temp++;
-      else set_temp--;
-    } else {
-      adjust_time(newPosition > oldPosition);
-    }
-    oldPosition = newPosition;
-  }
-  control_sauna();
+  // Make things pretty
   render_ui();
 }
